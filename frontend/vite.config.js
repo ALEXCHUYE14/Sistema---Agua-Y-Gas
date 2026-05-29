839a0b2b7249476089094be947b5b530f7849eb9
Miguel Alexander Flores Chuye <floreschuyem@gmail.com>
2026-05-29 17:50:24 -0500

    Sistema - AGUA Y GAS

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: "autoUpdate",
-      includeAssets: ["favicon.ico"],
+      includeAssets: ["favicon.svg"],
       manifest: {
         name: "AquaGas Delivery",
         short_name: "AquaGas",
